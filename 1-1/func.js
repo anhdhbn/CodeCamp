d0e2617de78364849873840fc64ad66b7f40dc8e
Anh DH <anhdhbn@gmail.com>
2018-03-16 18:44:57 +0700

    create edit

--- 1-1/func.js
+++ 1-1/func.js
@@ -1,14 +1,72 @@
+var btnClose = document.getElementsByClassName("close");
+var list = document.querySelectorAll("#myList li");
+var tab = [], liIndex;
+
+for(var i = 0; i < btnClose.length; i++)
+{
+    tab.push(list[i].innerHTML);
+}
+
+for(var i = 0; i < btnClose.length; i++){
+            
+    btnClose[i].onclick = function(){
+        
+        liIndex = tab.indexOf(this.parentNode.innerHTML);
+        //console.log(this.parentNode.innerHTML + " INDEX = " + liIndex);
+        this.parentNode.parentNode.removeChild(this.parentNode);
+    };
+    
+}
+
+for(var i = 0; i < list.length; i++)
+{
+    list[i].onclick = function()
+    {
+        if(this.className == "completed")
+        {
+            this.className = "";
+        }
+        else
+        {
+            this.className = "completed";
+        }
+    }
+}
+
+
 var button = document.getElementById("add");
     button.addEventListener('click', function(){
     
-    var data = document.getElementById("text-data").value;
     var node = document.createElement("li");  
-    console.log(data);
-    var textNode = document.createTextNode(data);
-    var subBtn = document.createElement('BUTTON');
-    subBtn.appendChild(document.createTextNode("x"));
-    subBtn.className = "lBtn";
-    node.appendChild(textNode);
+    var subBtn = document.createElement('span');
+    subBtn.appendChild(document.createTextNode("×"));
+    subBtn.className = "close";
+    subBtn.onclick = function(){
+        
+        liIndex = tab.indexOf(this.parentNode.innerHTML);
+        this.parentNode.parentNode.removeChild(this.parentNode);
+    };
+    
+
+    node.innerHTML = document.getElementById("text-data").value;
     node.appendChild(subBtn);
+    
+    node.onclick = function()
+    {
+        if(node.className == "completed")
+        {
+            node.className = "";
+        }
+        else
+        {
+            node.className = "completed";
+        }
+    }
+
+
+    tab.push(node.innerHTML);
+
     document.getElementById("myList").appendChild(node);
-})
+});
+
+
