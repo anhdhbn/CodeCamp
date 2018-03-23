6d78506d3d72e7c47def67133f54cee1acfa5381
Anh DH <anhdhbn@gmail.com>
2018-03-23 19:08:46 +0700

    add event click

--- 3-1/src/Components/TodosApp.js
+++ 3-1/src/Components/TodosApp.js
@@ -2,19 +2,62 @@ import React, { Component } from 'react';
 import '../style.css';
 
 class Todo extends Component {
+
+    onClickTodo(event) {
+        var target = event.target;
+
+        var tag = target.tagName;
+
+        if (tag === 'LI') {
+            //handleToggleTodo(target);
+            console.log("clicked li");
+        }
+
+        if (tag === 'SPAN') {
+            //handleRemoveTodo(target);
+            console.log("clicked span");
+        }
+    }
     render(){
-        return(
-            <li id="0">asd<span className="close">x</span></li>
+        return(          
+            <li key={this.key} id={this.props.id} onClick={this.onClickTodo}>{this.props.content}<span className="close" >x</span></li>
         );
     }
 }
 
 class TodosList extends Component {
+
+
+    createLi = () =>{
+        const contents = ["Uet", "Code", "Camp"];
+        let listLi = [];
+        for (let i = 0; i < contents.length; i++) { 
+              listLi.push(<Todo key={i} id={i} content={contents[i]}/>);
+        }   
+        return listLi;
+    }
+
     render(){
         return(
             <ul id="mylist" className="content">
-                <Todo/>
+                 {this.createLi()}
             </ul>
+        );
+    }
+}
+
+class Input extends Component {
+    render(){
+        return(
+            <input type="text" id="inputAdd" placeholder="Title..."/>
+        );
+    }
+}
+
+class Button extends Component {
+    render(){
+        return(
+            <span id="btnAdd">Add</span>
         );
     }
 }
@@ -24,8 +67,8 @@ class Header extends Component {
         return(
                 <div id="nav" className="header">
                     <h1 style={{margin: 5 + 'px'}} >My To Do List</h1>
-                    <input type="text" id="inputAdd" placeholder="Title..."/>
-                    <span id="btnAdd">Add</span>
+                    <Input/>
+                    <Button/>
                 </div>
         );
     }
